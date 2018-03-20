093e3c7d7dc5d2b8859771e375996c3f8cf32f28
jiawei11 <jiawei11@staff.sina.com.cn>
2018-03-20 23:17:36 +0800

    Feature:实现酷炫的顶部栏 http://blog.csdn.net/qq_20785431/article/details/52374310
    Commiter: zjw_swun
    
    Reviewer: null

--- app/src/main/java/com/zjw/mykotlinproject/MainActivity.kt
+++ app/src/main/java/com/zjw/mykotlinproject/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.zjw.mykotlinproject
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
+
+        toolbar.setTitleTextColor(Color.GREEN)
+        toolbar.setTitle("AppBarLayout")
+        setSupportActionBar(toolbar)
+
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        recyclerview.setLayoutManager(LinearLayoutManager(this))
+        recyclerview.setAdapter(ContentAdapter())
+
+        //通过CollapsingToolbarLayout设置title
+        collapsing_toolbar_layout.title = "AppBarLayout"
+        //通过CollapsingToolbarLayout修改字体颜色
+        collapsing_toolbar_layout.setExpandedTitleColor(Color.WHITE)//设置还没收缩时状态下字体颜色
+        collapsing_toolbar_layout.setCollapsedTitleTextColor(Color.RED)//设置收缩后Toolbar上字体的颜色
+
+    }
+
+    private inner class ContentAdapter : RecyclerView.Adapter<ContentAdapter.ContentHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentAdapter.ContentHolder {
+            return ContentHolder(LayoutInflater.from(this@MainActivity).inflate(android.R.layout.simple_list_item_1, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: ContentAdapter.ContentHolder, position: Int) {
+            holder.itemTv.text = "item"
+        }
+
+        override fun getItemCount(): Int {
+            return 35
+        }
+
+        internal inner class ContentHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+            val itemTv: TextView
+
+            init {
+                itemTv = itemView.findViewById<View>(android.R.id.text1) as TextView
+            }
+        }
     }
 }
